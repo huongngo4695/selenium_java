1ec3df7140e7a5f67c4d16dae7c239f7cb3a514e
huongngo4695 <huongngo461995@gmail.com>
2025-05-07 15:02:39 +0700

    Bai67. Data Type

---
+++ src/Bai67/Bai2.java
@@ -0,0 +1,10 @@
+package Bai67;
+
+public class Bai2 {
+
+    public static void main(String[] args) {
+         float a= 7.5F;
+         float b= 3.8F;
+        System.out.println("Area = " + (a*b));
+    }
+}
---
+++ src/Bai67/Bai3.java
@@ -0,0 +1,9 @@
+package Bai67;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        String name;
+        name = "Automation Testing";
+        System.out.println("Hello " + name);
+    }
+}
